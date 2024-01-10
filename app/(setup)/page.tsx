2196b418744fc213bd0c3c4fcaae6002d47e4878
AbdulWahabRaza123 <abdulwahabraza123@gmail.com>
2024-01-10 20:39:31 +0500

    stored profile info in prisma

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,4 +1,20 @@
+import { redirect } from "next/navigation";
+import { initialProfile } from "@/lib/initial-profile";
+import { db } from "@/lib/db";
 const SetupPage = async () => {
+  const profile = await initialProfile();
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+  if (server) {
+    redirect(`/server/${server.id}`);
+  }
   return <div>Create a Server</div>;
 };
 
